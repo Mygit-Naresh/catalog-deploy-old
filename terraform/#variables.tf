6258b489210414bc0818349fabf378c5cef3aead
naresh <Naresh@gmail.com>
2024-05-04 15:41:06 +0530

    catalogue config with tfvars

--- terraform/#variables.tf
+++ terraform/#variables.tf
@@ -1,41 +1,41 @@
-variable "tags" {
-  default = {
-    component = "catalogue"
-  }
-}
-variable "common_tags" {
-  type = map(string)
-  default = {
-    Createdby   = "Terraform",
-    Costcenter  = "FIN-005-HYD-CLOUD-AWS",
-    Admin_email = "admin.useterraform@gmail.com"
-  }
-}
-variable "project" {
-  default = "useterraform"
-}
-variable "environment" {
-  default = "dev"
-}
-variable "zone_id" {
-  default = "Z101265833JA5X90XBKK8"
-}
-variable "zone_name" {
-  default = "eternaltrainings.online"  
-}
-variable "instance_type" {
-  default = "t2.micro" 
-}
+# variable "tags" {
+#   default = {
+#     service = "catalogue"
+#   }
+# }
+# variable "common_tags" {
+#   type = map(string)
+#   default = {
+#     Createdby   = "Terraform",
+#     Costcenter  = "FIN-005-HYD-CLOUD-AWS",
+#     Admin_email = "admin.useterraform@gmail.com"
+#   }
+# }
+# variable "project" {
+#   default = "useterraform"
+# }
+# variable "environment" {
+#   default = "dev"
+# }
+# variable "zone_id" {
+#   default = "Z101265833JA5X90XBKK8"
+# }
+# variable "zone_name" {
+#   default = "eternaltrainings.online"  
+# }
+# variable "instance_type" {
+#   default = "t2.micro" 
+# }
 
-variable "priority" {
-  default = "10" 
-}
-variable "name" {
-  default = "catalogue"
-}
-variable "port_number" {
-  default = "8080"
-}
-variable "app_version" {
+# variable "priority" {
+#   default = "10" 
+# }
+# variable "name" {
+#   default = "catalogue"
+# }
+# variable "port_number" {
+#   default = "8080"
+# }
+# variable "app_version" {
   
-}
+# }
